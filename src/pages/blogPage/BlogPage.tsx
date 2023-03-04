eacae04f233045c241b9834aacd0f0c10661f9db
Naglis <naglis1134@gmail.com>
2023-03-04 17:15:27 +0100

    blog page looking decent

--- src/pages/blogPage/BlogPage.tsx
+++ src/pages/blogPage/BlogPage.tsx
@@ -1,37 +1,47 @@
 import { useEffect, useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-const readmePath = 'posts/1-marvel-karo-vertybes.md';
+
 function BlogPage() {
   const [text, setText] = useState("");
-  const fetchMarkdown = () => {
-  fetch(readmePath)
-  .then(response => {
-    return response.text()
-  })
-  .then(txt => {
-    setText(txt);
-  })
-}
-useEffect(()=>{fetchMarkdown()}, []);
+
+  const fetchMarkdown = (readmePath: string) => {
+    fetch(readmePath)
+      .then((response) => {
+        return response.text();
+      })
+      .then((txt) => {
+        setText(txt);
+      });
+  };
+
+  useEffect(() => {
+    fetchMarkdown("posts/1-marvel-karo-vertybes.md");
+  }, []);
+
   return (
     <>
-      <h1>Blog</h1>
+      <header
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <h1>Blog</h1>
+
+        <h3>
+          <a href="/">Home</a>
+        </h3>
+
+        <h3>
+          <a href="/#/contacts">Contacts</a>
+        </h3>
+      </header>
       <br />
-      <h2>MENU</h2>
-      <ul>
-        <li>
-          <h3>
-            <a href="/">Home</a>
-          </h3>
-        </li>
-        <li>
-          <h3>
-            <a href="/#/contacts">Contacts</a>
-          </h3>
-        </li>
-      </ul>
       <br />
-      <ReactMarkdown>{text}</ReactMarkdown>
+      <div style={{ paddingLeft: "20%", paddingRight: "20%" }}>
+        <ReactMarkdown>{text}</ReactMarkdown>
+      </div>
       <br />
       <br />
       <footer>Naglis/Audrius 2023</footer>
