9463e91e338e78710c1bf3c84315cb4cdcf4b333
Naglis <naglis1134@gmail.com>
2023-04-08 04:05:33 +0200

    test download button

--- src/pages/writePage/WritePage.tsx
+++ src/pages/writePage/WritePage.tsx
@@ -3,6 +3,9 @@ function WritePage() {
     <>
       <h3 style={{ paddingTop: "5%", textAlign: "center" }}>
         <em>More coming soon...</em>
+        <a href="logo.png" download>
+          click to download the logo
+        </a>
       </h3>
     </>
   );
