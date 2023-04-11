dcaec015951f982ecfa34e95b27bf95c2fe22093
Naglis <naglis1134@gmail.com>
2023-04-11 20:26:48 +0200

    add fizbangos

--- src/pages/teachPage/TeachPage.tsx
+++ src/pages/teachPage/TeachPage.tsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
 function TeachPage() {
+  const [text, setText] = useState("Loading...");
+
+  const fetchMarkdown = (readmePath: string) => {
+    fetch(readmePath)
+      .then((response) => {
+        return response.text();
+      })
+      .then((txt) => {
+        setText(txt);
+      });
+  };
+
+  useEffect(() => {
+    fetchMarkdown(
+      "https://raw.githubusercontent.com/naglissul/blog-posts/main/fizika-8-kl-bangos.md"
+    );
+  }, []);
   return (
     <>
       <main>
@@ -19,6 +39,7 @@ function TeachPage() {
           <a href="https://sodas.ugdome.lt/viesieji-puslapiai/7300">
             LT education programme till 2024-08-31
           </a>
+          <ReactMarkdown>{text}</ReactMarkdown>
         </section>
         <aside></aside>
       </main>
