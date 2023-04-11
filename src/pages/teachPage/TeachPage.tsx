931d51d5b528a3d08562bb3aa49106c24a74604c
Naglis <naglis1134@gmail.com>
2023-04-11 20:21:51 +0200

    add text

--- src/pages/teachPage/TeachPage.tsx
+++ src/pages/teachPage/TeachPage.tsx
@@ -9,7 +9,16 @@ function TeachPage() {
           rules as of a teacher and writing summaries/tutorials about school
           maths/physics according to the national education programme in
           Lithuania. After that maybe improve my, as a math and physics tutor,
-          work. Everything will be listed in this page
+          work. Everything will be listed in this page.
+          <ul>
+            <li>Overleaf - slides...with more, with papildomi...</li>
+            <li>Kahoot</li>
+            <li>Dictionary for definitions EN-LT (or platform for that)</li>
+          </ul>
+          Links:
+          <a href="https://sodas.ugdome.lt/viesieji-puslapiai/7300">
+            LT education programme till 2024-08-31
+          </a>
         </section>
         <aside></aside>
       </main>
