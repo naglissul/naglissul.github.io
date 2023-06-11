85a2a90dd418d7a1d0429100a669ccb052982fcd
Naglis <naglis1134@gmail.com>
2023-06-11 10:39:06 +0200

    Add snow page

---
+++ src/pages/writePage/TheFirstSnowPage.tsx
@@ -0,0 +1,16 @@
+export default function TheFirstSnowPage() {
+  return (
+    <section>
+      <h1>This gonna be a page for the book called "Pirmasis sniegas"</h1>
+      <ul>
+        <li>Cover</li>
+        <li>Sypnosis</li>
+        <li>Bio?</li>
+        <li>Excerpts & sample chapters</li>
+        <li>Reviews & testimonials</li>
+        <li>How to get info</li>
+        <li>Media kit</li>
+      </ul>
+    </section>
+  );
+}
