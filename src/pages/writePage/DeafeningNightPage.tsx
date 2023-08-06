b39c9ba0a4d0b04390ed50631c14c62999ddd628
Naglis <naglis1134@gmail.com>
2023-08-07 00:40:48 +0300

    Add deafeningnight file

--- src/pages/writePage/DeafeningNightPage.tsx
+++ src/pages/writePage/DeafeningNightPage.tsx
@@ -1,8 +1,15 @@
+import Reading from "./components/Reading";
+
 export default function DeafeningNightPage() {
   return (
     <section>
       Čia bus patalpinta informacija apie siaubo trupmą istoriją "Kurtinančiai
       tyli naktis dvipusiame name arba Ką padarysi..."
+      <Reading
+        name="Kurtinančiai
+        tyli naktis dvipusiame name arba Ką padarysi..."
+        src="assets/readings/other/deafening-night.pdf"
+      />
       {/* <h1>
         This gonna be a page for the book called "Kurtinančiai tyli naktis
         dvipusiame name arba Ką padarysi..."
