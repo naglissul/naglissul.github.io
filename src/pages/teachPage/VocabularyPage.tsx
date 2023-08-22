220477990f2ab64cb42e0f764e9641ee07c69294
Naglis <naglis1134@gmail.com>
2023-08-22 11:10:12 +0300

    add keywords voc

--- src/pages/teachPage/VocabularyPage.tsx
+++ src/pages/teachPage/VocabularyPage.tsx
@@ -9,7 +9,11 @@ function VocabularyPage() {
       <p>Not reviewed by other people (only me) </p>
       <p>
         For a new word or a better translation suggestions please email me{" "}
-        <a href="mailto:naglis.suliokas@gmail.com">naglis.suliokas@gmail.com</a>
+        <a href="mailto:naglis.suliokas@gmail.com">naglis.suliokas@gmail.com</a>{" "}
+        or create pull request at{" "}
+        <a href="https://github.com/naglissul/naglissul.github.io/blob/main/src/pages/teachPage/voc.json">
+          github
+        </a>
       </p>
       <hr />
       <table>
@@ -44,6 +48,22 @@ function VocabularyPage() {
         programų sistemoms I/IIId, DM - diskrečioji matematika, AT - algoritmų
         teorija, PP - procedūrinis programavimas (C) 2. MMT - VU Matematika ir
         matematikos taikymai 1kursas
+      </p>
+      <h5>Keywords for google:</h5>
+      <p style={{ fontSize: "5px" }}>
+        lietuviskai angliskai fizika matematika kompiuteriu mokslas Fizikoje
+        Fizikos Angliškai angliškai fizikoje fizikos matematikos matke matkė
+        matieka kompiuterių mokslas lietuviškai į lietuvių kalbą į lietuvių į
+        anglų į anglų kalbą i anglu i lietuviu i anglu kalba i lietuviu kalba
+        vertimas isversti zodynas išversti žodynas mokslinis žodynas terminų
+        žodynas terminai terminu zodynas fizikos zodynas matematikos zodynas
+        matematikos terminai fizikos terminai lt en lt-en lietuviu-anglu
+        {voc.map((word) => (
+          <p>
+            {word.lt} angliskai {word.lt} angliškai {word.en} lietuviskai{" "}
+            {word.en} lietuviškai {word.nl} in het engels{" "}
+          </p>
+        ))}
       </p>
     </section>
   );
